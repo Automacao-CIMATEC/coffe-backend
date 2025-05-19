63ecd88eea0912140dcbb47c97e88860205fb126
Davih de Andrade <davih.santos@fieb.org.br>
2025-05-19 16:44:55 -0300

    Implementação preliminar de um publisher MQTT
    Feat:
        - Implementação de um publisher MQTT simples, que permite escrever tópicos a partir de um broker mosquitto

---
+++ src/main/java/org/coffee/testes/mqtt/MqttPublisher.java
@@ -0,0 +1,39 @@
+package org.coffee.testes.mqtt;
+
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+public class MqttPublisher {
+    public static void main(String[] args) {
+        String broker = "tcp://localhost:1883"; // MQTT broker address
+        String topic = "test";                  // Topic to publish to
+        String message = "Hello from Java!";    // Message content
+
+        try {
+            // Create an MQTT client
+            MqttClient client = new MqttClient(broker, MqttClient.generateClientId());
+
+            // Connect options
+            MqttConnectOptions options = new MqttConnectOptions();
+            options.setAutomaticReconnect(true);
+
+            // Connect to broker
+            client.connect(options);
+            System.out.println("Connected to broker: " + broker);
+
+            // Publish message
+            MqttMessage mqttMessage = new MqttMessage(message.getBytes());
+            mqttMessage.setQos(1); // Quality of Service level 1
+            client.publish(topic, mqttMessage);
+            System.out.println("Published to '" + topic + "': " + message);
+
+            // Disconnect
+            client.disconnect();
+            System.out.println("Disconnected");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
